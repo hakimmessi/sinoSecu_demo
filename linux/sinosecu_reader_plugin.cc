0021c7a4df15ac73638747a96313127c6bcd8127
hakimmessi <hakeemmessi@outlook.com>
2025-05-22 14:25:38 +0200

    again

--- linux/sinosecu_reader_plugin.cc
+++ linux/sinosecu_reader_plugin.cc
@@ -3,3 +3,61 @@
 //
 
 #include "sinosecu_reader_plugin.h"
+#include <flutter_linux/flutter_linux.h>
+#include <iostream>
+#include <dlfcn.h>
+
+typedef int (*InitIDCardFunc)(const wchar_t*, int, const wchar_t*);
+
+void* libHandle = nullptr;
+InitIDCardFunc InitIDCard = nullptr;
+
+SinosecuReaderPlugin::SinosecuReaderPlugin() {}
+
+SinosecuReaderPlugin::~SinosecuReaderPlugin() {
+    if (libHandle) {
+        dlclose(libHandle);
+    }
+}
+
+void SinosecuReaderPlugin::HandleMethodCall(
+        const flutter::MethodCall<std::string> &call,
+        std::unique_ptr<flutter::MethodResult<std::string>> result) {
+    if (call.method_name().compare("initialize") == 0) {
+        if (!libHandle) {
+            libHandle = dlopen("libIDCard.so", RTLD_LAZY);
+            if (!libHandle) {
+                result->Error("LOAD_ERROR", dlerror());
+                return;
+            }
+
+            InitIDCard = (InitIDCardFunc)dlsym(libHandle, "InitIDCard");
+            if (!InitIDCard) {
+                result->Error("SYM_ERROR", dlerror());
+                return;
+            }
+        }
+
+        int ret = InitIDCard(L"426911010110763248", 0, L".");
+        result->Success(std::to_string(ret));
+    } else {
+        result->NotImplemented();
+    }
+}
+
+void SinosecuReaderPlugin::RegisterWithRegistrar(FlPluginRegistrar* registrar)
+ {
+     auto channel = std::make_unique<flutter::MethodChannel<std::string>>(
+             flutter_plugin_registrar_get_messenger(registrar),
+                     "sinosecu_reader",
+                     &flutter::StandardMethodCodec::GetInstance());
+
+    auto plugin = std::make_unique<SinosecuReaderPlugin>();
+
+    channel->SetMethodCallHandler(
+            [plugin_pointer = plugin.get()](const auto &call, auto result) {
+                plugin_pointer->HandleMethodCall(call, std::move(result));
+            });
+
+    registrar->AddPlugin(std::move(plugin));
+}
--- linux/sinosecu_reader_plugin.h
+++ linux/sinosecu_reader_plugin.h
@@ -4,5 +4,27 @@
 
 #ifndef SIM_DISPENSER_DEMO_APP_SINOSECU_READER_PLUGIN_H
 #define SIM_DISPENSER_DEMO_APP_SINOSECU_READER_PLUGIN_H
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_linux.h>
 
+#include <memory>
+
+class SinosecuReaderPlugin : public flutter::Plugin {
+public:
+    static void RegisterWithRegistrar(FlPluginRegistrar* registrar);
+
+
+    SinosecuReaderPlugin();
+    virtual ~SinosecuReaderPlugin();
+
+    // Disallow copy and assign.
+    SinosecuReaderPlugin(const SinosecuReaderPlugin&) = delete;
+    SinosecuReaderPlugin& operator=(const SinosecuReaderPlugin&) = delete;
+
+private:
+    // Handle method calls from Dart.
+    void HandleMethodCall(
+            const flutter::MethodCall<std::string> &method_call,
+            std::unique_ptr<flutter::MethodResult<std::string>> result);
+};
 #endif //SIM_DISPENSER_DEMO_APP_SINOSECU_READER_PLUGIN_H
