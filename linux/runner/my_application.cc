97a015836ce5d917dd9451656951bb699792c918
hakimmessi <hakeemmessi@outlook.com>
2025-05-23 10:05:31 +0200

    bugss

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -1,114 +1,71 @@
-#include "my_application.h"
-
+#include "sinosecu_reader_plugin.h"
 #include <flutter_linux/flutter_linux.h>
-#ifdef GDK_WINDOWING_X11
-#include <gdk/gdkx.h>
-#endif
+#include <glib-object.h>
+#include <string.h>
+#include <iostream>
+#include <dlfcn.h>
 
-#include "flutter/generated_plugin_registrant.h"
-#include "../sinosecu_reader_plugin.h"
-#include <flutter/plugin_registrar_manager.h>
+#define SINOSECU_READER_PLUGIN(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), sinosecu_reader_plugin_get_type(), SinosecuReaderPlugin))
 
-
-struct _MyApplication {
-    GtkApplication parent_instance;
-    char** dart_entrypoint_arguments;
+struct _SinosecuReaderPlugin {
+    GObject parent_instance;
 };
 
-G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
+G_DEFINE_TYPE(SinosecuReaderPlugin, sinosecu_reader_plugin, g_object_get_type())
 
-static void my_application_activate(GApplication* application) {
-    MyApplication* self = MY_APPLICATION(application);
-    GtkWindow* window =
-            GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+typedef int (*InitIDCardFunc)(const wchar_t*, int, const wchar_t*);
 
-    gboolean use_header_bar = TRUE;
-#ifdef GDK_WINDOWING_X11
-    GdkScreen* screen = gtk_window_get_screen(window);
-  if (GDK_IS_X11_SCREEN(screen)) {
-    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-      use_header_bar = FALSE;
-    }
-  }
-#endif
-    if (use_header_bar) {
-        GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-        gtk_widget_show(GTK_WIDGET(header_bar));
-        gtk_header_bar_set_title(header_bar, "sinoSecu_demo");
-        gtk_header_bar_set_show_close_button(header_bar, TRUE);
-        gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+static void* libHandle = nullptr;
+static InitIDCardFunc InitIDCard = nullptr;
+
+static void method_call_cb(FlMethodChannel* channel,
+                           FlMethodCall* method_call,
+                           gpointer user_data) {
+    const gchar* method = fl_method_call_get_name(method_call);
+
+    if (strcmp(method, "initialize") == 0) {
+        if (!libHandle) {
+            libHandle = dlopen("libIDCard.so", RTLD_LAZY);
+            if (!libHandle) {
+                g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(
+                        fl_method_error_response_new("LOAD_ERROR", dlerror(), nullptr));
+                fl_method_channel_invoke_response(channel, method_call, response, nullptr);
+                return;
+            }
+
+            InitIDCard = (InitIDCardFunc)dlsym(libHandle, "InitIDCard");
+            if (!InitIDCard) {
+                g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(
+                        fl_method_error_response_new("SYM_ERROR", dlerror(), nullptr));
+                fl_method_channel_invoke_response(channel, method_call, response, nullptr);
+                return;
+            }
+        }
+
+        int ret = InitIDCard(L"426911010110763248", 0, L".");
+        char ret_str[16];
+        snprintf(ret_str, sizeof(ret_str), "%d", ret);
+        g_autoptr(FlValue) result = fl_value_new_string(ret_str);
+        g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(
+                fl_method_success_response_new(result));
+        fl_method_channel_invoke_response(channel, method_call, response, nullptr);
     } else {
-        gtk_window_set_title(window, "sinoSecu_demo");
+        g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(
+                fl_method_not_implemented_response_new());
+        fl_method_channel_invoke_response(channel, method_call, response, nullptr);
     }
-
-    gtk_window_set_default_size(window, 1280, 720);
-    gtk_widget_show(GTK_WIDGET(window));
-
-    g_autoptr(FlDartProject) project = fl_dart_project_new();
-    fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
-
-    FlView* view = fl_view_new(project);
-    gtk_widget_show(GTK_WIDGET(view));
-    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
-
-    fl_register_plugins(FL_PLUGIN_REGISTRY(view));
-
-    SinosecuReaderPlugin::RegisterWithRegistrar(
-            fl_plugin_registry_get_registrar_for_plugin(
-                    FL_PLUGIN_REGISTRY(view), "SinosecuReaderPlugin"));
-
-
-    );
-
-    gtk_widget_grab_focus(GTK_WIDGET(view));
 }
 
-static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
-    MyApplication* self = MY_APPLICATION(application);
-    self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
+void sinosecu_reader_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
+    g_autoptr(SinosecuReaderPlugin) plugin = SINOSECU_READER_PLUGIN(
+            g_object_new(sinosecu_reader_plugin_get_type(), nullptr));
 
-    g_autoptr(GError) error = nullptr;
-    if (!g_application_register(application, nullptr, &error)) {
-        g_warning("Failed to register: %s", error->message);
-        *exit_status = 1;
-        return TRUE;
-    }
+    g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
+    g_autoptr(FlMethodChannel) channel = fl_method_channel_new(
+            fl_plugin_registrar_get_messenger(registrar),
+            "sinosecu_reader",
+            FL_METHOD_CODEC(codec));
 
-    g_application_activate(application);
-    *exit_status = 0;
-
-    return TRUE;
-}
-
-static void my_application_startup(GApplication* application) {
-    G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
-}
-
-static void my_application_shutdown(GApplication* application) {
-    G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
-}
-
-static void my_application_dispose(GObject* object) {
-    MyApplication* self = MY_APPLICATION(object);
-    g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
-    G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
-}
-
-static void my_application_class_init(MyApplicationClass* klass) {
-    G_APPLICATION_CLASS(klass)->activate = my_application_activate;
-    G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
-    G_APPLICATION_CLASS(klass)->startup = my_application_startup;
-    G_APPLICATION_CLASS(klass)->shutdown = my_application_shutdown;
-    G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
-}
-
-static void my_application_init(MyApplication* self) {}
-
-MyApplication* my_application_new() {
-    g_set_prgname(APPLICATION_ID);
-    return MY_APPLICATION(g_object_new(my_application_get_type(),
-                                       "application-id", APPLICATION_ID,
-                                       "flags", G_APPLICATION_NON_UNIQUE,
-                                       nullptr));
+    fl_method_channel_set_method_call_handler(channel, method_call_cb, plugin, nullptr);
 }
